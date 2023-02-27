29f2c10f75fbfc05f85f4f68d96d71afe0216a10
humaiun <humaiunkobirr@gmail.com>
2023-02-28 04:13:56 +0600

    update footer & todoList

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -39,7 +39,7 @@ export default function Todo({ todo }) {
         )}
       </div>
 
-      <div className="select-none flex-1 line-through">{text}</div>
+      <div className={`select-none flex-1 ${completed && "line-through"}`}>{text}</div>
 
       <div
         onClick={() => handleColorChange(id, "green")}
